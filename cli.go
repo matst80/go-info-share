6f3b2245a9484ad4da4ab7d1639756057a872cac
matst80 <mats.tornberg@gmail.com>
2026-01-31 13:03:40 +0000

    Update CLI to accept --url flag and INFO_SERVER_URL env var

--- cli.go
+++ cli.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,16 +9,28 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("usage: cli <key> <value>")
+	var url string
+	flag.StringVar(&url, "url", "", "Base URL of the info server")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("usage: cli [--url BASE_URL] <key> <value>")
 		os.Exit(1)
 	}
 
-	key := os.Args[1]
-	value := os.Args[2]
+	key := args[0]
+	value := args[1]
 
-	url := fmt.Sprintf("http://localhost:8080/set?key=%s&value=%s", key, value)
-	resp, err := http.Post(url, "application/x-www-form-urlencoded", nil)
+	if url == "" {
+		url = os.Getenv("INFO_SERVER_URL")
+		if url == "" {
+			url = "http://localhost:8080"
+		}
+	}
+
+	fullURL := fmt.Sprintf("%s/set?key=%s&value=%s", url, key, value)
+	resp, err := http.Post(fullURL, "application/x-www-form-urlencoded", nil)
 	if err != nil {
 		fmt.Println("error:", err)
 		os.Exit(1)
